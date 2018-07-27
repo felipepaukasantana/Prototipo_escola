e964771762aeab1e0c365f9640619ad0bff21cdd
Felipe Santana <felipepauka@gmail.com>
2018-07-26 23:18:00 -0300

    front end com menu

--- front/br-com-escola-ui/src/app/app.module.ts
+++ front/br-com-escola-ui/src/app/app.module.ts
@@ -2,13 +2,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { MenuComponent } from './menu/menu.component';
+
+import { InputTextModule } from 'primeng/inputtext';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    MenuComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+
+    InputTextModule
   ],
   providers: [],
   bootstrap: [AppComponent]
